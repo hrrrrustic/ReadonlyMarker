dbb62e42a8aadd3324e4d526038d98ec1383398e
Vlad <hrrrrustic@gmail.com>
2021-01-04 11:48:45 +0300

    Add project files.

---
+++ ReadonlyMarker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadonlyMarker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
